43ed137658d1ff81c15cf25f2f3e7d40cc7d7e51
unknown <caceres.diego@tecnica7.edu.ar>
2025-05-05 19:58:10 -0300

    primer comit

---
+++ java/modulo.js
@@ -0,0 +1,7 @@
+const header = '<p class="encabezado">Recetas de Cocina</p>';
+const nav = '<ul><li><a href="index.html">Inicio</a></li><li><a href="pagina1.html">Postres</a></li><li><a href="pagina2.html">Comida Salada</a></li></ul>';
+const footer = '<p class="pie">&copy; 2025 Recetas de Cocina - Todos los derechos reservados</p>';
+
+document.querySelector('header').innerHTML = header;
+document.querySelector('nav').innerHTML = nav;
+document.querySelector('footer').innerHTML = footer;
