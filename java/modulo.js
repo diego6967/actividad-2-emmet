3b80bbe41e3f64a6e8a90fe7edd38d0cf72a5b6d
diego6967 <caceres.diego@tecnica7.edu.ar>
2025-05-07 19:56:16 -0300

    rer

--- java/modulo.js
+++ java/modulo.js
@@ -1,5 +1,5 @@
 const header = '<p class="encabezado">Recetas de Cocina</p>';
-const nav = '<ul><li><a href="index.html">Inicio</a></li><li><a href="pagina1.html">Postres</a></li><li><a href="pagina2.html">Comida Salada</a></li></ul>';
+const nav = '<ul><li><i class="fa-solid fa-utensils"></i><a href="index.html">Inicio</a></li><li><a href="pagina1.html">Postres</a></li><li><a href="pagina2.html">Comida Salada</a></li></ul>';
 const footer = '<p class="pie">&copy; 2025 Recetas de Cocina - Todos los derechos reservados</p>';
 
 document.querySelector('header').innerHTML = header;
