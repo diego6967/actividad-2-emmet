5444ddc4581ba784c5ac1640f5202bce674dc771
diego6967 <caceres.diego@tecnica7.edu.ar>
2025-05-07 19:51:41 -0300

    WE

--- java/modulo.js
+++ java/modulo.js
@@ -6,9 +6,3 @@ document.querySelector('header').innerHTML = header;
 document.querySelector('nav').innerHTML = nav;
 document.querySelector('footer').innerHTML = footer;
 
-  const toggleBtn = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  toggleBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
